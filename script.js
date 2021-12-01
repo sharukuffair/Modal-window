7b1ab1b864daec1e7e1b7e56a1b9a5a9faf43952
sharukuffair <73853475+sharuk-uffair@users.noreply.github.com>
2021-12-01 13:43:34 +0530

    Initial files added
    Files added via local disk.

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// keyboard Event Handler
+document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+});
